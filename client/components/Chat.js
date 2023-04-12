e523ff744a562d9e60a34c42883f12c8212d0f46
Aadhavan <aadhavanlenin@gmail.com>
2023-04-12 23:07:02 +0530

    scroll to bottom

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styles from './chat.module.css'
+import ScrollToBottom from 'react-scroll-to-bottom'
 // import { useRouter } from 'next/router'
 
 export default function Chat({ socket, userName, room, setLoggedIn }) {
@@ -23,6 +24,7 @@ export default function Chat({ socket, userName, room, setLoggedIn }) {
 				time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes(),
 			}
 			await socket.emit('sendMessage', messageData)
+			setCurrentMessage('')
 		}
 	}
 
@@ -68,38 +70,45 @@ export default function Chat({ socket, userName, room, setLoggedIn }) {
 			<div className={styles['wrapper']}>
 				<div className={styles['room-wrapper']}>
 					Room: {room}
-					<img src='/share.svg' />
+					<img
+						src='/share.svg'
+						onClick={() => {
+							navigator.clipboard.writeText(`http://localhost:3000/${room}`)
+						}}
+					/>
 				</div>
 				{/* <div className={styles['chat']}> */}
 				<div className={styles['chat-container']}>
 					<div className={styles['chat-wrapper']}>
-						{messageList.map((item, index) => (
-							<div key={index}>
-								{item.userName === userName ? (
-									<div className={styles['right-container']}>
-										<div className={styles['right-wrapper']}>
-											<div className={styles['right']}>
-												<div className={styles['message-box']}>{item.message}</div>
+						<ScrollToBottom className={styles['scroller']}>
+							{messageList.map((item, index) => (
+								<div key={index} className={styles['spacing']}>
+									{item.userName === userName ? (
+										<div className={styles['right-container']}>
+											<div className={styles['right-wrapper']}>
+												<div className={styles['right']}>
+													<div className={styles['message-box']}>{item.message}</div>
+													<img src={item.avatar} />
+												</div>
+												<span>
+													{item.userName}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{item.time}
+												</span>
+											</div>
+										</div>
+									) : (
+										<div className={styles['left-wrapper']}>
+											<div className={styles['left']}>
 												<img src={item.avatar} />
+												<div className={styles['message-box']}>{item.message}</div>
 											</div>
 											<span>
 												{item.userName}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{item.time}
 											</span>
 										</div>
-									</div>
-								) : (
-									<div className={styles['left-wrapper']}>
-										<div className={styles['left']}>
-											<img src={item.avatar} />
-											<div className={styles['message-box']}>{item.message}</div>
-										</div>
-										<span>
-											{item.userName}&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;{item.time}
-										</span>
-									</div>
-								)}
-							</div>
-						))}
+									)}
+								</div>
+							))}
+						</ScrollToBottom>
 					</div>
 					<div className={styles['send-wrapper']}>
 						<input
